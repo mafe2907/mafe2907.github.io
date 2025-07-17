df454063350a56c9788fe29c8f44649e091296e3
mafe2907 <mafecomemoco8@gmail.com>
2025-07-17 11:36:59 -0500

    borre logica

--- logica.js
+++ logica.js
@@ -1,5 +1,2 @@
-var elt = document.getElementById('calculator');
-var calculator = Desmos.GraphingCalculator(elt, {
-  expressions: true
-});
-calculator.setExpression({ id: 'parabola', latex: 'y = x^2' });
+
+</script>
